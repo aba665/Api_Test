a173fca60b7dae965c69519bd327af01cdcecbf3
aba665 <abissons@gmail.com>
2022-06-23 14:39:28 -0300

    ok

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from 'cors';
 import userController from "./controllers/userController.js";
 import { Autheticated } from "./middleware/autheticated.js";
+import 'dotenv/config';
 
 const app = express();
 
